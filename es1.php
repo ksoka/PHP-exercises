40966a74cd9bbbcd572094ae218a80c388e6c69a
Kalle Soukka <t9soka00@students.oamk.fi>
2020-01-30 12:15:10 +0200

    jee

---
+++ es1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+    <h2>Variables</h2>
+        <?php
+        $fname="Jim";
+        echo 'Hello '.$fname;
+        ?>
+    </body>
+</html>
