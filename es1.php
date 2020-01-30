2a57b1fe6395db53e859e0fcbddd2522e6bc8b4b
Kalle Soukka <t9soka00@students.oamk.fi>
2020-01-30 13:37:52 +0200

    page3

--- es1.php
+++ es1.php
@@ -16,11 +16,26 @@
         <p><?php print_r($fnames); ?></p>
         <p><?php echo 'First is '.$fnames[0]; ?></p>
 
-        <h2>Assosiative array</h2>
-<?php
-$assoc_fnames=array('fin'=>'Ruotsi', 'swe'=>'Sverige', 'eng'=>'Sveden');
-?>
-</p>
-<p> <?php echo 'The Finnish name for '.$assoc_fnames['eng'].' is '.$assoc_fnames['fin']; ?> </p>
+    <h2>Assosiative array</h2>
+        <?php
+        $assoc_fnames=array('fin'=>'Ruotsi', 'swe'=>'Sverige', 'eng'=>'Sveden');
+        ?>
+        </p>
+        <p> <?php echo 'The Finnish name for '.$assoc_fnames['eng'].' is '.$assoc_fnames['fin']; ?> </p>
+    <h2>Two dimensional array</h2>
+        <?php
+        $persons=array(
+        array('fn'=>'Jim', 'ln'=>'Smith'),
+        array('fn'=>'Lisa', 'ln'=>'Jones'),
+        array('fn'=>'Ann', 'ln'=>'Simpson')
+        );
+        ?>
+        <ul>
+        <?php
+        foreach ($persons as $row) {
+            echo '<li>'.$row['fn'].' '.$row['ln'].'</li>';
+        }
+        ?>
+        </ul>
     </body>
 </html>
