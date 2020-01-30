b46dd61da9cc57230f386d3d2e0da80da5dadb25
Kalle Soukka <t9soka00@students.oamk.fi>
2020-01-30 13:37:15 +0200

    page3

--- es1.php
+++ es1.php
@@ -9,5 +9,18 @@
         $fname="Jim";
         echo 'Hello '.$fname;
         ?>
+    <h2>Arrays</h2>
+        <?php
+        $fnames=array('Jim', 'Lisa', 'Ann');
+        ?>
+        <p><?php print_r($fnames); ?></p>
+        <p><?php echo 'First is '.$fnames[0]; ?></p>
+
+        <h2>Assosiative array</h2>
+<?php
+$assoc_fnames=array('fin'=>'Ruotsi', 'swe'=>'Sverige', 'eng'=>'Sveden');
+?>
+</p>
+<p> <?php echo 'The Finnish name for '.$assoc_fnames['eng'].' is '.$assoc_fnames['fin']; ?> </p>
     </body>
 </html>
